9dd8b386c2894b3c77291e92f16a2e7b2d9c390e
JoanM27 <142604590+JoanM27@users.noreply.github.com>
2026-05-10 15:03:48 -0500

    Creación de entorno del proyecto

---
+++ junior-404-game-jka/scripts/autoloads/PlayerData.gd
@@ -0,0 +1,7 @@
+extends Node
+var dinero: int = 500
+var inventario_items: Array = []
+var item_equipado = null  # será un Resource tipo Item
+
+func guardar_progreso(): pass
+func cargar_progreso(): pass
